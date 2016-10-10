c8161bd2ff53976165ec9c00cf326350cb923e42
afterwind <afterwinddesu@gmail.com>
2016-10-10 22:42:03 +0300

    Added filtering methods.

--- src/afterwind/lab1/controller/CandidateController.java
+++ src/afterwind/lab1/controller/CandidateController.java
@@ -1,6 +1,7 @@
 package afterwind.lab1.controller;
 
 import afterwind.lab1.entity.Candidate;
+import afterwind.lab1.repository.Repository;
 
 /**
  * Stratul de "Controller" pentru entitatea "Candidate"
@@ -26,5 +27,50 @@ public class CandidateController extends AbstractController<Candidate> {
         candidate.setName(name);
         candidate.setAddress(address);
         candidate.setTel(tel);
+    }
+
+    /**
+     * Filtrare dupa nume
+     * @param name stringul cu care este comparat numele prin startsWith
+     * @return un repository care contine toate datele filtrate
+     */
+    public Repository<Candidate> filterByName(String name) {
+        Repository<Candidate> repo = new Repository<>(Candidate.class);
+        for (Candidate c : repo.getData()) {
+            if (c != null && c.getName().startsWith(name)) {
+                repo.add(c);
+            }
+        }
+        return repo;
+    }
+
+    /**
+     * Filtrare dupa numarul de telefon
+     * @param telephone stringul cu care este comparat numele prin startsWith
+     * @return un repository care contine toate datele filtrate
+     */
+    public Repository<Candidate> filterByTelephone(String telephone) {
+        Repository<Candidate> repo = new Repository<>(Candidate.class);
+        for (Candidate c : repo.getData()) {
+            if (c != null && c.getTel().startsWith(telephone)) {
+                repo.add(c);
+            }
+        }
+        return repo;
+    }
+
+    /**
+     * Filtrare dupa adresa
+     * @param address stringul cu care este comparat numele prin startsWith
+     * @return un repository care contine toate datele filtrate
+     */
+    public Repository<Candidate> filterByAddress(String address) {
+        Repository<Candidate> repo = new Repository<>(Candidate.class);
+        for (Candidate c : repo.getData()) {
+            if (c != null && c.getAddress().startsWith(address)) {
+                repo.add(c);
+            }
+        }
+        return repo;
     }
 }
--- src/afterwind/lab1/controller/SectionController.java
+++ src/afterwind/lab1/controller/SectionController.java
@@ -1,6 +1,7 @@
 package afterwind.lab1.controller;
 
 import afterwind.lab1.entity.Section;
+import afterwind.lab1.repository.Repository;
 
 /**
  * Stratul de "Controller" pentru entitatea "Section"
@@ -24,5 +25,38 @@ public class SectionController extends AbstractController<Section> {
     public void updateSection(Section section, String name, int nrLoc) {
         section.setName(name);
         section.setNrLoc(nrLoc);
+    }
+
+    /**
+     * Filtrare dupa nume
+     * @param name stringul cu care este comparat numele prin startsWith
+     * @return un repository care contine toate datele filtrate
+     */
+    public Repository<Section> filterByName(String name) {
+        Repository<Section> repo = new Repository<>(Section.class);
+        for (Section s : repo.getData()) {
+            if (s != null && s.getName().startsWith(name)) {
+                repo.add(s);
+            }
+        }
+        return repo;
+    }
+
+    /**
+     * Filtrare dupa numar de locuri
+     * @param nrLoc transa
+     * @param lower daca verifica daca e mai mic sau mai mare decat transa
+     * @return un repository care contine toate datele filtrate
+     */
+    public Repository<Section> filterByNrLoc(int nrLoc, boolean lower) {
+        Repository<Section> repo = new Repository<>(Section.class);
+        for (Section s : repo.getData()) {
+            if (s != null) {
+                if (lower && s.getNrLoc() <= nrLoc || !lower && s.getNrLoc() >= nrLoc) {
+                    repo.add(s);
+                }
+            }
+        }
+        return repo;
     }
 }
